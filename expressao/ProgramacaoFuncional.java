da45990cc734c6716514c1b35110eeb3c4fe300a
Raphael-Sammy <133025924+Raphael-Sammy@users.noreply.github.com>
2023-11-21 08:51:33 -0300

    Add files via upload

---
+++ expressao/ProgramacaoFuncional.java
@@ -0,0 +1,26 @@
+package sem.expressao;
+
+import java.util.*;
+
+public class ProgramacaoFuncional {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV",900.0));
+        list.add(new Product("Notebook",1200.0));
+        list.add(new Product("Tablet",450.0));
+
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(comp);
+
+        for(Product p : list){
+            System.out.println(p);
+        }
+    }
+}
